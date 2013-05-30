f29582613d958b834f5d97e4db2a26a676f9cf90
jkroso <jkroso@gmail.com>
2013-05-31 11:23:23 +1200

    add a nice dirty compiler
    this can replace a manual build step in a lot of cases. probably
    all my use cases at least

---
+++ compile-js.js
@@ -0,0 +1,103 @@
+
+var path = require('path')
+	, join = path.join
+	, dirname = path.dirname
+	, fs = require('fs')
+	, Graph = require('sourcegraph')
+	, Compiler = require('bigfile')
+	, JS = require('sourcegraph/src/plugins/javascript').types[0]
+	, cheerio = require('cheerio')
+
+module.exports = function(base, opts){
+	var graph = new Graph()
+		.use('nodeish')
+		.use('mocha')
+		.use('css')
+		.use('stylus')
+		.use('jade')
+
+	var build = new Compiler(path.basename(base))
+		.plugin('nodeish')
+		.plugin('css')
+		.plugin('stylus')
+		.plugin('jade')
+		.use('transform')
+		.use('quick-path-shorten')
+		.use('development')
+		.use('umd')
+		.use(function(code){
+			this.end(code)
+		})
+
+	return function(req, res, next){
+		var path = join(base, req.url)
+		var stat
+		try {
+			stat = fs.statSync(path)
+		} catch (e) {
+			return next(e)
+		}
+
+		// javascript file
+		if (/\.js$/.test(path) && stat.isFile()) {
+			return graph.clear()
+				.add(path)
+				.then(values)
+				.read(function(files){
+					build.end = function(code){
+						res.setHeader('Content-Type', 'application/javascript')
+						res.end(code)
+					}
+					build.entry = path
+					build.send(files)
+				})
+		}
+
+		var index = join(path, 'index.html')
+		if (stat.isDirectory() && fs.existsSync(index)) {
+			path = index
+			stat = fs.statSync(path)
+		}
+
+		// handle embeded js
+		if (/\.html$/.test(path) && stat.isFile()) {
+			var html = fs.readFileSync(path)
+			var $ = cheerio.load(html)
+			var scripts = $('script')
+			var script = scripts[0]
+			if (scripts.length == 1 && typeof script.attribs.src != 'string') {
+				graph.clear()
+				script = script.children[0].data
+				var file = graph.graph[path] = {
+					path: path,
+					'last-modified': stat.mtime,
+					text: script,
+					parents: [],
+					children: [],
+					aliases: [],
+					base: dirname(path)
+				}
+				file.requires = JS.prototype.requires.call(file)
+				return graph.trace(file).then(function(){
+					build.entry = path
+					build.end = function send(code){
+						scripts[0].children[0].data = code
+							// TODO: fix this weird encoding issue properly
+							.replace(/ƒ/g, '$fn$')
+						res.setHeader('Content-Type', 'text/html')
+						res.end($.html())
+					}
+					build.send(values(graph.graph))
+				})
+			}
+		}
+		
+		next()
+	}
+}
+
+function values(obj){
+	var vals = []
+	for (var k in obj) vals.push(obj[k])
+	return vals
+}
